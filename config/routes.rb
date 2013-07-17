61b50d9e80b66f938b5791cfc682d16a52256ce3
jeff <junyi@routeam.com>
2013-07-17 15:11:07 -0400

    Validate

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,6 @@ SampleApp::Application.routes.draw do
   root to: 'static_pages#home'
 
   match '/signup',  to: 'users#new'
-
   match '/about', :to => "static_pages#about"
   match '/contact', :to => "static_pages#contact"
   match '/help', :to => "static_pages#help"
