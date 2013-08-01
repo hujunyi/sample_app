f2d5e3de72d1d093e64cfdc21516e1ec284049c7
jeff <junyi@routeam.com>
2013-08-01 16:25:38 -0400

    Signup error message

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 SampleApp::Application.routes.draw do
-  get "users/new"
   root to: 'static_pages#home'
+  resources :users
 
   match '/signup',  to: 'users#new'
   match '/about', :to => "static_pages#about"
