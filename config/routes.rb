fc5773a8219d605c1fe4b4e8cc82fdf9d6c8309a
jeff <junyi@routeam.com>
2013-08-05 23:53:35 -0400

    8.1.2

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,15 @@
 SampleApp::Application.routes.draw do
   root to: 'static_pages#home'
   resources :users
+  resources :sessions, only: [:new, :create, :destroy]
+
 
   match '/signup',  to: 'users#new'
   match '/about', :to => "static_pages#about"
   match '/contact', :to => "static_pages#contact"
   match '/help', :to => "static_pages#help"
+  match '/signin',  to: 'sessions#new'
+  match '/signout', to: 'sessions#destroy', via: :delete
   # get "static_pages/about"
   # get "static_pages/contact"
   
