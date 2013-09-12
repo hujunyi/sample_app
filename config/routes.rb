14041ec7b56c123a0911178176da2546a10d908b
hujunyi <hujunyi1990@gmail.com>
2013-09-12 01:33:34 -0400

    8.2

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,9 @@ SampleApp::Application.routes.draw do
   resources :users
   resources :sessions, only: [:new, :create, :destroy]
 
+  match '/signup',  to: 'users#new'
+  match '/signin',  to: 'sessions#new'
+  match '/signout', to: 'sessions#destroy', via: :delete
 
   match '/signup',  to: 'users#new'
   match '/about', :to => "static_pages#about"
